d17ed9c18461cdceecbcab6c3a541df694bbfad8
Guido De Caro <gdecaro@itba.edu.ar>
2022-06-18 14:24:26 -0300

    changes

---
+++ README.md
@@ -0,0 +1,20 @@
+# Readme Trabajo Especial XML
+
+## Archivos requeridos:
+
+Para ejecutar el programa correctamente, se debe contar con los siguientes archivos en el directorio donde se está trabajando:
+
+### generateXml.sh
+### getter.xq
+### convert_to_csv.xsl
+
+Además se debe tener instalado Java, Xerces, y Saxon.
+
+## Ejecutar el archivo tp.sh:
+
+getter.sh se ejecuta de la siguiente manera:
+
+### ./getter.sh
+
+Al ingresar dicho comando se notificará el proceso de lectura y al finalizar será generado un archivo .csv con la información de salida en el mismo directorio. 
+En caso de que el archivo se encuentre vacío, en  “intermediate.xml” se encontrarán los errores ocurridos durante la ejecución.
--- generateXml.sh
+++ generateXml.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-arid=${1#*=}
+artist_id=${1#*=}
 
-curl https://musicbrainz.org/ws/2/artist/${artist_id}?inc=works > "artist_info.xml"
+curl "https://musicbrainz.org/ws/2/artist/${artist_id}?inc=works" > "artist_info.xml"
 echo "artist_info.xml generated."
 
-curl https://musicbrainz.org/ws/2/recording?query=arid:${artist_id}&limit=1000 > "recordings_info.xml"
+curl "https://musicbrainz.org/ws/2/recording?query=arid:${artist_id}&limit=1000" > "recordings_info.xml"
 echo "recordings_info.xml generated."
 
-java net.sf.saxon.Query getter.xq west=$west south=$south east=$east north=$north > "intermediate.xml"
-echo "intermediate.xml generated."
+java net.sf.saxon.Query getter.xq arid=$artist_id > "artist_data.xml"
+echo "artist_data.xml generated."
 
 java net.sf.saxon.Transform -s:./intermediate.xml -xsl:./convert_csv.xsl -o:output.csv
 echo "output.csv generated, check your directory."

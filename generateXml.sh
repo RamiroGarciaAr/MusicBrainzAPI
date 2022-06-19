97b0d460a74734370e5bb37a0202c5ffc200a3db
brunibaumgart <95543587+brunibaumgart@users.noreply.github.com>
2022-06-18 23:03:18 -0300

    Update generateXml.sh

--- generateXml.sh
+++ generateXml.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# artist_id=${1#*=}
+artist_id=${1#*=}
 
 
-curl -o artist_info.xml "https://musicbrainz.org/ws/2/artist/$1?inc=works" 
+curl -o artist_info.xml "https://musicbrainz.org/ws/2/artist/${artist_id}?inc=works" 
 echo "artist_info.xml generated."
 
-curl -o recordings_info.xml "https://musicbrainz.org/ws/2/recording?query=arid:$1&limit=1000" 
+curl -o recordings_info.xml "https://musicbrainz.org/ws/2/recording?query=arid:${artist_id}&limit=1000" 
 echo "recordings_info.xml generated."
 
 # java net.sf.saxon.Query extract_data.xq > "artist_data.xml"

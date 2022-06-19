1bf5e1fdcf3d016303261a2e32af11308e9e6a3d
Guido De Caro <gdecaro@itba.edu.ar>
2022-06-19 12:54:24 -0300

    remove

--- tpe.sh
+++ tpe.sh
@@ -10,6 +10,9 @@ echo "artist_info.xml generated."
 curl -o recordings_info.xml "https://musicbrainz.org/ws/2/recording?query=arid:${artist_id}&limit=1000" 
 echo "recordings_info.xml generated."
 
+java net.sf.saxon.Transform -s:artist_info.xml -xsl:remove_namespace.xsl -o:artist_info.xml
+java net.sf.saxon.Transform -s:recordings_info.xml -xsl:remove_namespace.xsl -o:recordings_info.xml
+
 java net.sf.saxon.Query ./extract_data.xq > "artist_data.xml"
 echo "artist_data.xml generated."
 

ac8fbbe2e0fad5ff720d4dc8a277d688a272cbba
Guido De Caro <gdecaro@itba.edu.ar>
2022-06-19 11:46:33 -0300

    changes

--- tpe.sh
+++ tpe.sh
@@ -9,9 +9,9 @@ echo "artist_info.xml generated."
 curl -o recordings_info.xml "https://musicbrainz.org/ws/2/recording?query=arid:${artist_id}&limit=1000" 
 echo "recordings_info.xml generated."
 
-java net.sf.saxon.Query -o artist_data.xml extract_data.xq
+java net.sf.saxon.Query -o artist_data.xml extract_data.xq 
 echo "artist_data.xml generated."
 
-java net.sf.saxon.Transform -s:./intermediate.xml -xsl:./convert_csv.xsl -o:output.csv
+java net.sf.saxon.Transform -s:./artist_data.xml -xsl:./convert_csv.xsl -o:output.csv
 echo "output.csv generated, check your directory."
-echo "If 'output.csv' is empty, check the error output in 'intermediate.xml'."
+echo "If 'output.csv' is empty, check the error output in 'artist_data.xml'."

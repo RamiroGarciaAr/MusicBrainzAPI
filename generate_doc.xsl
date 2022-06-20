85f88c430a2bc6ed33bc0e31caf6fceb4ae019a6
Guido De Caro <gdecaro@itba.edu.ar>
2022-06-20 18:54:39 -0300

    txt

--- generate_doc.xsl
+++ generate_doc.xsl
@@ -1,8 +1,8 @@
 <?xml version="1.0"?>
 <xsl:Transform>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="2.0">
-<xsl:template match=/artist_data/artist>
-= <xsl:value-of select=name/>
+<xsl:template match="/artist_data/artist">
+= <xsl:value-of select="name"/>
 <xsl:if test="disambiguation">
 * Disambiguation:<xsl:value-of select="disambiguation"/>
 </xsl:if>
--- tpe.sh
+++ tpe.sh
@@ -13,9 +13,9 @@ echo "recordings_info.xml generated."
 java net.sf.saxon.Transform -s:artist_info.xml -xsl:remove_namespace.xsl -o:artist_info.xml
 java net.sf.saxon.Transform -s:recordings_info.xml -xsl:remove_namespace.xsl -o:recordings_info.xml
 
-java net.sf.saxon.Query extract_data.xq > "artist_data.xml"
+java net.sf.saxon.Query ./extract_data.xq > "artist_data.xml"
 echo "artist_data.xml generated."
 
-java net.sf.saxon.Transform -s:artist_data.xml -xsl:generate_doc.xsl > "artist_page.adoc"
+java net.sf.saxon.Transform -s:artist_data.xml -xsl:generate_doc.xsl > "artist_page.txt"
 echo "artist_page.adoc generated, check your directory."
 echo "If 'output.csv' is empty, check the error output in 'artist_data.xml'."

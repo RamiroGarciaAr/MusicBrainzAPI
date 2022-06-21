636d3d0b7219776caf376ec6dbed2294858c29e4
Guido De Caro <gdecaro@itba.edu.ar>
2022-06-20 23:50:04 -0300

    peron

--- tpe.sh
+++ tpe.sh
@@ -9,27 +9,30 @@ if [ "$#" != "1" ]; then
     exit 1
 else
     echo "Processing data please wait..."
+    if grep -Fq "$artist_id" artists_list.xml
+    then
+        echo "Id found processing still waiting..."
+        curl -o artist_info.xml "https://musicbrainz.org/ws/2/artist/${artist_id}?inc=works" 
+        echo "artist_info.xml generated."
+
+        curl -o recordings_info.xml "https://musicbrainz.org/ws/2/recording?query=arid:${artist_id}&limit=1000" 
+        echo "recordings_info.xml generated."
+
+        java net.sf.saxon.Transform -s:artist_info.xml -xsl:remove_namespace.xsl -o:artist_info.xml
+        java net.sf.saxon.Transform -s:recordings_info.xml -xsl:remove_namespace.xsl -o:recordings_info.xml
+
+        java net.sf.saxon.Query ./extract_data.xq > "artist_data.xml"
+        echo "artist_data.xml generated."
+
+    else
+        echo 
+        "<artist_data>
+            <error>El id indicado no existe en 'artists_list.xml'</error>
+        </artist_data>" > "artist_data.xml"
+    fi
 fi
 
 
-if grep -Fq "$artist_id" artists_list.xml
-then
-    echo "Id found processing still waiting..."
-    curl -o artist_info.xml "https://musicbrainz.org/ws/2/artist/${artist_id}?inc=works" 
-    echo "artist_info.xml generated."
-
-    curl -o recordings_info.xml "https://musicbrainz.org/ws/2/recording?query=arid:${artist_id}&limit=1000" 
-    echo "recordings_info.xml generated."
-
-    java net.sf.saxon.Transform -s:artist_info.xml -xsl:remove_namespace.xsl -o:artist_info.xml
-    java net.sf.saxon.Transform -s:recordings_info.xml -xsl:remove_namespace.xsl -o:recordings_info.xml
-
-    java net.sf.saxon.Query ./extract_data.xq > "artist_data.xml"
-    echo "artist_data.xml generated."
-
-else
-    echo "<error>El id indicado no existe en 'artists_list.xml'</error>" > "artist_data.xml"
-fi
 
 java net.sf.saxon.Transform -s:artist_data.xml -xsl:generate_doc.xsl -o:artist_page.adoc
 echo "artist_page.adoc generated, check your directory."

11350772784c044d5473eecb927d3ce2cc8faae3
Guido De Caro <gdecaro@itba.edu.ar>
2022-06-20 23:45:27 -0300

    check

--- tpe.sh
+++ tpe.sh
@@ -2,7 +2,7 @@
 
 artist_id=${1#*=}
 
-# isInFile=$(cat "./artists_list.xml" | grep -c "$artist_id")
+rm -f artist_info.xml recordings_info.xml artist_data.xml
 
 if [ "$#" != "1" ]; then
     echo "You must provide only one artist_id."
@@ -11,33 +11,25 @@ else
     echo "Processing data please wait..."
 fi
 
-# if [ $isInFile -eq 0 ]; then
-#    echo "Esta"
-# else
-#    echo "El id indicado no existe en 'artists_list.xml'" > "artist_data.xml"
-# fi
 
 if grep -Fq "$artist_id" artists_list.xml
 then
-    echo "Id found processing data please wait..."
+    echo "Id found processing still waiting..."
+    curl -o artist_info.xml "https://musicbrainz.org/ws/2/artist/${artist_id}?inc=works" 
+    echo "artist_info.xml generated."
+
+    curl -o recordings_info.xml "https://musicbrainz.org/ws/2/recording?query=arid:${artist_id}&limit=1000" 
+    echo "recordings_info.xml generated."
+
+    java net.sf.saxon.Transform -s:artist_info.xml -xsl:remove_namespace.xsl -o:artist_info.xml
+    java net.sf.saxon.Transform -s:recordings_info.xml -xsl:remove_namespace.xsl -o:recordings_info.xml
+
+    java net.sf.saxon.Query ./extract_data.xq > "artist_data.xml"
+    echo "artist_data.xml generated."
+
 else
     echo "<error>El id indicado no existe en 'artists_list.xml'</error>" > "artist_data.xml"
-    exit 1
 fi
-
-rm -f artist_info.xml recordings_info.xml artist_data.xml
-
-curl -o artist_info.xml "https://musicbrainz.org/ws/2/artist/${artist_id}?inc=works" 
-echo "artist_info.xml generated."
-
-curl -o recordings_info.xml "https://musicbrainz.org/ws/2/recording?query=arid:${artist_id}&limit=1000" 
-echo "recordings_info.xml generated."
-
-java net.sf.saxon.Transform -s:artist_info.xml -xsl:remove_namespace.xsl -o:artist_info.xml
-java net.sf.saxon.Transform -s:recordings_info.xml -xsl:remove_namespace.xsl -o:recordings_info.xml
-
-java net.sf.saxon.Query ./extract_data.xq > "artist_data.xml"
-echo "artist_data.xml generated."
 
 java net.sf.saxon.Transform -s:artist_data.xml -xsl:generate_doc.xsl -o:artist_page.adoc
 echo "artist_page.adoc generated, check your directory."
